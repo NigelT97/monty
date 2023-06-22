d5898fd0a3f5e4c7201f1197f151047e253d3bd6
PStar <nigelmj97@gmail.com>
2023-06-22 22:00:12 +0200

    fix

--- add_nop.c
+++ add_nop.c
@@ -47,23 +47,13 @@ void xfreestack(stack_t *head)
 	}
 }
 /**
- * f_pop - prints the top
- * @head: stack head
- * @counter: line number
-*/
-void xfpop(stack_t **head, unsigned int counter)
+  *f_nop- nothing
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void xfnop(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
-
-	if (*head == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		xfreestack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	*head = h->next;
-	free(h);
+	(void) counter;
+	(void) head;
 }
